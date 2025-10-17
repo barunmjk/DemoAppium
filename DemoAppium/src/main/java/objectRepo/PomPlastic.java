4e1067f2d8d76ea9344eb4c5e237b50c9a94c6c1
Fleek IT <Fleek IT@DESKTOP-HBPR2UE.bbrouter>
2025-10-17 15:45:45 +0530

    this is build test

--- DemoAppium/src/main/java/objectRepo/PomPlastic.java
+++ DemoAppium/src/main/java/objectRepo/PomPlastic.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
 public class PomPlastic {
      WebDriver driver;
      public PomPlastic(WebDriver driver){
@@ -34,7 +36,7 @@ public class PomPlastic {
 			 return name;
 	 }
 		 
-		 @FindBy(  xpath="//android.widget.EditText[@resource-id='ion-input-3']")
+		 @FindBy(xpath = "//android.widget.EditText[@resource-id='ion-input-3']")
 	     private WebElement phNum;
 		 public WebElement getPhNum() {
 			 return phNum;
@@ -49,6 +51,90 @@ public class PomPlastic {
 		 public WebElement getContinueBtn() {
 			 return continueBtn;
 	 } 
-		 
-		 
+		 @AndroidFindBy(uiAutomator ="new UiSelector().resourceId('ion-input-2')")
+		 private WebElement countryCode;
+		 public WebElement getCountryCode() {
+			 return countryCode;
+	 } 
+		 @FindBy(xpath = "//android.widget.EditText[@resource-id='ion-input-4']")
+		 private WebElement dob;
+		 public WebElement getDOB() {
+			 return dob;
+		 }
+		  @FindBy(xpath = "//android.view.View[@text='Select a year']")
+		  private WebElement selYear;
+		  public WebElement getSelYear() {
+			  return selYear;
+		  }
+		  @FindBy(xpath = "//android.widget.Button[@text='DONE']")
+		  private WebElement done;
+		  public WebElement getDone() {
+			  return done;
+		  }
+		  @FindBy(xpath ="//android.widget.Button[@resource-id='ion-sel-0']")
+		  private WebElement gender;
+		  public WebElement getGender() {
+			  return gender;
+		  }
+		  @FindBy(xpath ="//android.widget.RadioGroup/android.view.View[2]/android.view.View/android.view.View")
+		  private WebElement male;
+		  public WebElement getMale() {
+			  return male;
+		  }
+		  @FindBy(xpath="//android.widget.EditText[@resource-id='ion-input-5']")
+		  private WebElement pass;
+		  public WebElement getPass() {
+			  return pass;
+		  }
+		  @FindBy(xpath ="//android.widget.EditText[@resource-id='ion-input-6']")
+		  private WebElement cpass;
+		  public WebElement getCPass() {
+			  return cpass;
+		  }
+		  @FindBy(xpath="//android.widget.CheckBox/android.view.View/android.widget.Image")
+		  private WebElement checkBox;
+		  
+		  public WebElement getCheckBox() {
+			  return checkBox;
+		  }
+		  @FindBy(xpath = "//android.view.View[@resource-id='root']/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.widget.Image")
+		  private WebElement camlogo;
+		  public WebElement getcamlogo() {
+			  return camlogo;
+		  }
+		  @FindBy(xpath ="//android.widget.Button[@text='Take photo']")
+		  private WebElement takephoto;
+		  public WebElement getTakePhoto() {
+			  return takephoto;
+		  }
+		  @FindBy(xpath ="//android.widget.ImageView[@content-desc='Shutter']")
+		  private WebElement clickpic;
+		  public WebElement getClickpic() {
+			  return clickpic;
+		  }
+		  @FindBy(xpath ="//android.widget.ImageButton[@content-desc='Done']")
+		  private WebElement picdone;
+		  public WebElement getPicdone() {
+			  return picdone;
+		  }
+		  @FindBy(xpath ="//android.widget.Button[@text='Save photo']")
+		  private WebElement savepic;
+		  public WebElement getSavePic() {
+			  return savepic;
+		  }
+		  @FindBy(xpath="//android.widget.Button[@text='Register']")
+		  private WebElement register;
+		  public WebElement getRegister() {
+			  return register;
+		  }
+		  @FindBy(xpath ="//android.widget.Button[@text='Do this later']")
+		  private WebElement dothislater;
+		   
+		  public WebElement getDoThisLater() {
+			 
+			return dothislater;
+		  }
+
+	 
+		  
 }
--- DemoAppium/src/test/java/pixel9a/Pixel_test.java
+++ DemoAppium/src/test/java/pixel9a/Pixel_test.java
@@ -4,24 +4,32 @@ package pixel9a;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Arrays;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.idealized.Javascript;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import objectRepo.PomPlastic;
 
 public class Pixel_test {
-	 
+	 public static AndroidDriver driver;
     @Test
      public void app() throws MalformedURLException, InterruptedException{
     	 DesiredCapabilities caps=new DesiredCapabilities();
@@ -34,7 +42,7 @@ public class Pixel_test {
 	        caps.setCapability("appium:activity", "org.plasticbank.app.MainActivity");
 	        caps.setCapability("autoGrantPermissions", true);
 	        URL url =new URL("http://127.0.0.1:4723/");
-            AndroidDriver driver=new AndroidDriver(url,caps);
+            driver=new AndroidDriver(url,caps);
              
  
      }
@@ -81,10 +89,10 @@ public class Pixel_test {
   	        caps.setCapability("appium:activity", "org.plasticbank.app.MainActivity");
   	        caps.setCapability("autoGrantPermissions", true);
   	        URL url =new URL("http://127.0.0.1:4723/");
-  	       AndroidDriver driver=new AndroidDriver(url,caps);
+  	       driver=new AndroidDriver(url,caps);
   	       PomPlastic pom =new PomPlastic(driver);
- 	       
-           WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(300));
+ 	       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(200));
+           WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(160));
            wait.until(ExpectedConditions.elementToBeClickable(pom.getPlasticApp()));
            Thread.sleep(4000);
             pom.getPlasticApp().click();
@@ -98,7 +106,103 @@ public class Pixel_test {
          wait.until(ExpectedConditions.visibilityOf(pom.getName()));
          pom.getName().sendKeys("bran");
          Thread.sleep(4000);
-         wait.until(ExpectedConditions.visibilityOf(pom.getPhNum()));
-         pom.getPhNum().sendKeys("987654321");
+         
+         tap(590,1200);
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_2));
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_3));
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_2));
+         driver.pressKey(new KeyEvent(AndroidKey.DIGIT_4));
+         Thread.sleep(4000);
+         
+         pom.getDOB().click();
+         Thread.sleep(4000);
+         
+         Thread.sleep(4000);
+        
+        scroll(499,1265,504,1396);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(800,1256,790,1353);
+        scroll(252,1265,257,1411);
+        
+        Thread.sleep(4000);
+        pom.getDone().click();
+        Thread.sleep(4000);
+        pom.getGender().click();
+        Thread.sleep(4000);
+        pom.getMale().click();
+        Thread.sleep(4000);
+        pom.getPass().sendKeys("Bran@123");
+        Thread.sleep(4000);
+        pom.getCPass().sendKeys("Bran@123");
+        Thread.sleep(4000);
+        pom.getCheckBox().click();
+        Thread.sleep(4000);
+        pom.getcamlogo().click();
+        Thread.sleep(4000);
+        pom.getTakePhoto().click();
+        Thread.sleep(4000);
+        pom.getClickpic().click();
+        Thread.sleep(6000);
+        pom.getPicdone().click();
+        Thread.sleep(6000);
+        pom.getSavePic().click();
+        wait.until(ExpectedConditions.elementToBeClickable(pom.getRegister()));
+        pom.getRegister().click();
+        Thread.sleep(4000);
+        pom.getDoThisLater().click();
+        
+//        tap(150,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+//        tap(315,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+//        tap(465,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+//        tap(616,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+//        tap(790,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+//        tap(931,1154);
+//        driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1));
+        
     }
+      
+      
+      public void tap(int x, int y) throws InterruptedException {
+    	  Thread.sleep(4000); PointerInput fingert = new
+    	  PointerInput(PointerInput.Kind.TOUCH, "fingert"); Sequence scrollt = new
+    	  Sequence(fingert, 1);
+    	  scrollt.addAction(fingert.createPointerMove(Duration.ofMillis(0),
+    	  PointerInput.Origin.viewport(), x, y));
+    	  scrollt.addAction(fingert.createPointerDown(0));
+    	  scrollt.addAction(fingert.createPointerMove(Duration.ofMillis(100),
+    	  PointerInput.Origin.viewport(), x, y));
+    	  scrollt.addAction(fingert.createPointerUp(0));
+    	 driver.perform(Arrays.asList(scrollt)); 
+    	  }
+      
+      public void scroll(int x, int y,int p, int q ){
+    	  PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+    	  Sequence swipe = new Sequence(finger, 0);
+
+    	 
+    	  swipe.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), x, y));
+    	  swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+    	  swipe.addAction(finger.createPointerMove(Duration.ofMillis(6000), PointerInput.Origin.viewport(), p, q));
+    	  swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+    	  driver.perform(Arrays.asList(swipe));
+    	  }
+      
 }
